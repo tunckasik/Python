c4c14b824001efbfae8331316902287e61cfecd1
tunckasik <tunckasikali@gmail.com>
2022-08-10 22:12:26 -0400

    Refreshed

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,26 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def head():
+    
+    return "<h1>Hello Bronze World!</h1>"
+
+@app.route("/second")
+def second():
+    return "This is my second page"
+
+@app.route("/third")
+def third():
+    return "<h2> This is third page </h2>"
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f'Id of this page is {id}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
